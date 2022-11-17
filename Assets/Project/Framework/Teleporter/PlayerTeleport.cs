eeea89ffddf9ede4384a7e7cabe999d3e15bd7c8
RafaRossi <rossi.rafael.proenca@gmail.com>
2022-11-17 00:57:16 -0300

    feat: player teleport

---
+++ Assets/Project/Framework/Teleporter/PlayerTeleport.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerTeleport : MonoBehaviour
+{
+    [SerializeField] private CharacterController controller;
+    [SerializeField] private Transform targetPosition;
+
+    [SerializeField] private bool keepSameDistance = true;
+
+    public void Teleport()
+    {
+        controller.enabled = false;
+
+        var position = controller.transform.position;
+
+        var objectTransformPosition = transform.position;
+        
+        
+        var difference = keepSameDistance ? new Vector3(position.x - objectTransformPosition.x,
+            position.y - objectTransformPosition.y,
+            position.z - objectTransformPosition.z) : Vector3.zero;
+        
+        position = (targetPosition.position + difference);
+        controller.transform.position = position;
+
+        controller.enabled = true;
+    }
+}
