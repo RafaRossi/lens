44d809d53dba1f293d5202f04408e6069d16de40
RafaRossi <rossi.rafael.proenca@gmail.com>
2023-06-09 18:09:49 -0300

    Create run_tests.bat

---
+++ run_tests.bat
@@ -0,0 +1,3 @@
+"C:\Program Files\Unity\Hub\Editor\2021.3.18f1\Editor\Unity.exe" -nographics -quit -runTests -batchmode -projectPath "%CD%"  -testResults="%CD%\unity_unittests_results.xml" -testPlatform editmode
+
+exit /b 0
